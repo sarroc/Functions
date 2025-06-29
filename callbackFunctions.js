f38ada74c9ba59e4abf98246473f4dccbf81ae45
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-29 14:41:45 +0200

    pass a function into another function; callback

---
+++ callbackFunctions.js
@@ -0,0 +1,18 @@
+// callbacks are simply functions that are passed into other functions to be invoked within the outer function.
+
+function child() {
+    alert('Hey, wanna play?')
+}
+
+setTimeout(child, 2000)
+// executes funtion after a set amount of time, in this case 5000 ms
+
+// ananymous function
+setTimeout(function () {
+    alert("Let's get going!");
+}, 5000);
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', function() {
+    alert("Good to see you here!")
+})
