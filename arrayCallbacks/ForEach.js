ff2fdad1fa868b3f7c904d09002f5a9312ad793f
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-29 17:30:01 +0200

    forEach method

--- arrayCallbacks/ForEach.js
+++ arrayCallbacks/ForEach.js
@@ -10,3 +10,39 @@ function printTriple(n) {
 
 numbers.forEach(printTriple);
 // anonymous function, is only needed once
+
+numbers.forEach(function(num, idx) {
+    console.log(idx, num);
+})
+
+const books =[{
+    title: 'Fourth Wing',
+    author: ['Rebecca Yarros'],
+    rating: 5
+    },
+    {
+    title: 'ACOTAR',
+    author: ['Sarah J. Maas'],
+    rating: 4.75
+    },
+    {
+    title: 'Shatter Me',
+    author: ['Tahereh Mafi'],
+    rating: 4.5
+    },
+    {
+    title: 'Grace and Fury',
+    author: ['Tracy Bangart'],
+    rating: 4.75
+    }
+]
+
+books.forEach(function(book) {
+    console.log(book.title.toUpperCase())
+})
+
+for (let book of books) {
+    console.log(book.title.toUpperCase())
+}
+// both versions doing the same thing. forEach is a newer function. for loop calls a block and forEach an array.
+
